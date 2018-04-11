d7bc864afaf9e2ecfeb05b862450f65f61e7d5c0
emmapatton <G00364733@gmit.ie>
2018-04-11 21:48:13 +0100

    Addition of a number of numerical calculations using mean, max, min, median, std, corr functions

--- irismean.py
+++ irismean.py
@@ -11,12 +11,46 @@ from pprint import pprint
 
 irisstats = pd.read_csv("data/iris.csv", header = None, names = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Name"])
 
-x = irisstats.describe()
+# describe_result = irisstats.describe()
 
-print(irisstats.sample(5))
-# pprint(x)
+# twodp = describe_result.round(2)
 
+# print(twodp)
 
+# print(irisstats.sample(5))
 
+# mean_result = irisstats.mean()
+
+# meandp = mean_result.round(2)
+
+# print(meandp)
+
+# median_result = irisstats.median()
+
+# print(median_result)
+
+# standev_result = irisstats.std()
+
+# standp = standev_result.round(2)
+
+# print(standp)
+
+# max_result = irisstats.max(numeric_only=1)
+
+# print(max_result)
+
+# min_result = irisstats.min(numeric_only=1)
+
+# print(min_result)
+
+# spearman_result = irisstats.corr(method='spearman')
+
+# print(spearman_result) #https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
+
+# correl_result = irisstats.corr(method='pearson')
+
+# print(correl_result)
+
+#https://en.wikipedia.org/wiki/Correlation_coefficient
 
 
