7de74422ad77bdd226b84b04e7e2057e3c080e35
emmapatton <G00364733@gmit.ie>
2018-04-29 15:49:20 +0100

    addition of comments to code and added pngs for readme

--- iris_stats_data.py
+++ iris_stats_data.py
@@ -1,24 +1,39 @@
-# Emma Patton, 2018-04-06
-# Stats of iris data set using pandas
-# Data obtained from: http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
 
-#Import the pandas library as pd
+# Emma Patton, Programming and Scripting Project - 2018 
+# Analysis of Iris Stats Data Set
+# The data was investigated using a number of mathematical functions 
+# The data has also been grouped and visually represented in histograms and scatter plots 
+
+# Iris Stats Data Data obtained from: http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pprint import pprint 
 
+#Reading: 
+# https://pandas.pydata.org/
+# https://matplotlib.org/
+
+
+# I imported pandas, numpy and matplotlib in order to use built in functions to analyse 
+# the data, group it and represent it visually. 
+
+
+# I first creatd a heading variable and passed a list of strings representing the four
+# measurements taken in 
+
 heading = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width"]
 
-#Load in the data with'read_csv()'
+# I then loaded in the data using pd.read_csv()
 irisstats = pd.read_csv("data/iris.csv", header = None, names = [*heading, "Species"]) #I needed to add "Species" on to the end of my header so I used the spread operator (*)
 
 
+irisstats_group_bys = irisstats.groupby(['Species']) #I wanted to separate out nmy data by species so I used the pandas function .groupby 
 
-irisstats_group_bys = irisstats.groupby(['Species'])
 
-
-setosa_df = irisstats_group_bys.get_group('Iris-setosa')
+setosa_df = irisstats_group_bys.get_group('Iris-setosa') #I passed in each group that I wanted using .getgroup 
 setosa_df.name = 'Iris-setosa'
 
 versicolor_df = irisstats_group_bys.get_group('Iris-versicolor')
@@ -27,6 +42,9 @@ versicolor_df.name = 'Iris-versicolor'
 virginica_df = irisstats_group_bys.get_group('Iris-virginica')
 virginica_df.name = 'Iris-virginica'
 
+
+# I then created a function called getIrisStats to analyse each psecies group. I wanted to
+# find out the count, mean, median etc. I then print these individually to the terminal. 
 
 def getIrisStats(species):
     count = species.count()
@@ -68,7 +86,7 @@ def showScatterIris(setosa, versicolor, virginica, column_1, column_2):
     plt.ylabel(column_2)
 
     plt.scatter(x1, y1, c='b')
-    plt.scatter(x2, y2, c='o') 
+    plt.scatter(x2, y2, c='r') 
     plt.scatter(x3, y3, c='g')
 
     plt.show()
@@ -103,39 +121,12 @@ showHistogramIris(setosa_df, versicolor_df, virginica_df, "Petal Length")
 showHistogramIris(setosa_df, versicolor_df, virginica_df, "Petal Width")
 
 
+# Reading from: 
+# https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.hist.html
+# https://pandas.pydata.org/pandas-docs/stable/visualization.html
+# https://matplotlib.org/api/_as_gen/matplotlib.pyplot.hist.html
 
 
-# describe_result = irisstats.describe()
-
-# twodp = describe_result.round(2)
-
-# print(twodp)
-
-# print(irisstats.sample(5))
-
-# mean_result = irisstats.mean()
-
-# meandp = mean_result.round(2)
-
-# print(meandp)
-
-# median_result = irisstats.median()
-
-# print(median_result)
-
-# standev_result = irisstats.std()
-
-# standp = standev_result.round(2)
-
-# print(standp)
-
-# max_result = irisstats.max(numeric_only=1)
-
-# print(max_result)
-
-# min_result = irisstats.min(numeric_only=1)
-
-# print(min_result)
 
 # spearman_result = irisstats.corr(method='spearman')
 
